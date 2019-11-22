461b86e47fa6d9ca6a878fcb8ad3f7bb49267d5a
HQingChen <hc1988@nyu.edu>
2019-11-21 19:02:38 -0500

    AddRestartLogic

---
+++ Assets/Scripts/Objects/PlayerDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDetector : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject player = collision.gameObject;
+
+        if (player.CompareTag("Player"))
+        {
+            StartCoroutine(GameOver(player));
+        }
+    }
+
+    IEnumerator GameOver(GameObject player)
+    {
+        player.SetActive(false);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("StartScreen");
+    }
+}
