f9040eae61940323260d9a28e53513fb3f647fcb
Matthew Hantsbarger <m.hantsbarger@comcast.net>
2019-12-10 15:39:45 -0500

    start menu logic made applicable to all

--- Assets/Scripts/MenuNavigation.cs
+++ Assets/Scripts/MenuNavigation.cs
@@ -13,22 +13,27 @@ public class MenuNavigation : MonoBehaviour
     public const int POINTERXPOS = 400;
     public Image pointer;
 
+    public Text[] options;
     public Text option1;
     public Text option2;
     public Text option3;
 
-    private int numberOfOptions = 3;
+    // private int numberOfOptions = 3;
 
     private int selectedOption;
 
     // Use this for initialization
     void Start () {
-        selectedOption = 1;
-        option1.color = selectedColor;
-        option2.color = unselectedColor;
-        option3.color = unselectedColor;
+        selectedOption = 0;
+        foreach (Text option in options) {
+            option.color = unselectedColor;
+        }
+        options[0].color = selectedColor;
+        // option1.color = selectedColor;
+        // option2.color = unselectedColor;
+        // option3.color = unselectedColor;
 
-        pointer.transform.position = new Vector3(POINTERXPOS, option1.transform.position.y);
+        pointer.transform.position = new Vector3(POINTERXPOS, options[0].transform.position.y);
     }
 
     // Update is called once per frame
@@ -36,59 +41,70 @@ public class MenuNavigation : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.DownArrow) /*|| Controller input*/)
         { //Input telling it to go up or down.
             selectedOption += 1;
-            if (selectedOption > numberOfOptions) //If at end of list go back to top
+            if (selectedOption >= options.Length) //If at end of list go back to top
             {
-                selectedOption = 1;
+                selectedOption = 0;
             }
 
-            option1.color = unselectedColor;
-            option2.color = unselectedColor;
-            option3.color = unselectedColor;
-
-            switch (selectedOption) //Set the visual indicator for which option you are on.
-            {
-                case 1:
-                    option1.color = selectedColor;
-                    pointer.transform.position = new Vector3(POINTERXPOS, option1.transform.position.y);
-                    break;
-                case 2:
-                    option2.color = selectedColor;
-                    pointer.transform.position = new Vector3(POINTERXPOS, option2.transform.position.y);
-                    break;
-                case 3:
-                    option3.color = selectedColor;
-                    pointer.transform.position = new Vector3(POINTERXPOS, option3.transform.position.y);
-                    break;
+            foreach (Text option in options) {
+                option.color = unselectedColor;
             }
+            options[selectedOption].color = selectedColor;
+            pointer.transform.position = new Vector3(POINTERXPOS, options[selectedOption].transform.position.y);
+            // option1.color = unselectedColor;
+            // option2.color = unselectedColor;
+            // option3.color = unselectedColor;
+
+            // switch (selectedOption) //Set the visual indicator for which option you are on.
+            // {
+            //     case 1:
+            //         option1.color = selectedColor;
+            //         pointer.transform.position = new Vector3(POINTERXPOS, option1.transform.position.y);
+            //         break;
+            //     case 2:
+            //         option2.color = selectedColor;
+            //         pointer.transform.position = new Vector3(POINTERXPOS, option2.transform.position.y);
+            //         break;
+            //     case 3:
+            //         option3.color = selectedColor;
+            //         pointer.transform.position = new Vector3(POINTERXPOS, option3.transform.position.y);
+            //         break;
+            // }
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow) /*|| Controller input*/)
         { //Input telling it to go up or down.
             selectedOption -= 1;
-            if (selectedOption < 1) //If at end of list go back to top
+            if (selectedOption < 0) //If at end of list go back to top
             {
-                selectedOption = numberOfOptions;
+                selectedOption = options.Length-1;
             }
 
-            option1.color = unselectedColor;
-            option2.color = unselectedColor;
-            option3.color = unselectedColor;
-
-            switch (selectedOption) //Set the visual indicator for which option you are on.
-            {
-                case 1:
-                    option1.color = selectedColor;
-                    pointer.transform.position = new Vector3(POINTERXPOS, option1.transform.position.y);
-                    break;
-                case 2:
-                    option2.color = selectedColor;
-                    pointer.transform.position = new Vector3(POINTERXPOS, option2.transform.position.y);
-                    break;
-                case 3:
-                    option3.color = selectedColor;
-                    pointer.transform.position = new Vector3(POINTERXPOS, option3.transform.position.y);
-                    break;
+            foreach (Text option in options) {
+                option.color = unselectedColor;
             }
+            options[selectedOption].color = selectedColor;
+            pointer.transform.position = new Vector3(POINTERXPOS, options[selectedOption].transform.position.y);
+
+            // option1.color = unselectedColor;
+            // option2.color = unselectedColor;
+            // option3.color = unselectedColor;
+
+            // switch (selectedOption) //Set the visual indicator for which option you are on.
+            // {
+            //     case 1:
+            //         option1.color = selectedColor;
+            //         pointer.transform.position = new Vector3(POINTERXPOS, option1.transform.position.y);
+            //         break;
+            //     case 2:
+            //         option2.color = selectedColor;
+            //         pointer.transform.position = new Vector3(POINTERXPOS, option2.transform.position.y);
+            //         break;
+            //     case 3:
+            //         option3.color = selectedColor;
+            //         pointer.transform.position = new Vector3(POINTERXPOS, option3.transform.position.y);
+            //         break;
+            // }
         }
 
         if (Input.GetKeyDown(KeyCode.Space) ||  Input.GetKeyDown("joystick button 0")){
@@ -96,13 +112,13 @@ public class MenuNavigation : MonoBehaviour
 
             switch (selectedOption) //Set the visual indicator for which option you are on.
             {
-                case 1:
+                case 0:
                     sceneLoader.SceneLoader(5);
                     break;
-                case 2:
+                case 1:
                     sceneLoader.SceneLoader(1);
                     break;
-                case 3:
+                case 2:
                     sceneLoader.doExitGame();
                     break;
             }
